c0f10c0222abf45165ae07eb397edf1d8579643e
Halcolt <billtrinh19@gmail.com>
2023-12-30 20:18:24 +0700

    add new admin

--- UIUX/src/main/java/com/example/uiux/Main.java
+++ UIUX/src/main/java/com/example/uiux/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Main.primaryStage = primaryStage; // Assign the primaryStage to the static field
-        loadScene("HomeLayoutRemake.fxml");
+        loadScene("admin_TrangChu.fxml");
     }
 
     public static void loadScene(String fxmlFile) {
