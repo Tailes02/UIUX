47b94a8b47d016b6e92f5b5b1b16e628ba3ba6c9
Halcolt <billtrinh19@gmail.com>
2024-01-01 00:26:36 +0700

    add admin screen

--- UIUX/src/main/java/com/example/uiux/Main.java
+++ UIUX/src/main/java/com/example/uiux/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Main.primaryStage = primaryStage; // Assign the primaryStage to the static field
-        loadScene("admin_TrangChuRemake.fxml");
+        loadScene("admin_ThongTinTaiKhoan.fxml");
     }
 
     public static void loadScene(String fxmlFile) {
