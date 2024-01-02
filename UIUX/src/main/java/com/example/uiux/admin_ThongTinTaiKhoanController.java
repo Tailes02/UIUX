47866d9210abec6ad3d2f85c31ab1fad991a6fc0
Halcolt <billtrinh19@gmail.com>
2024-01-02 22:42:55 +0700

    nuke

--- UIUX/src/main/java/com/example/uiux/admin_ThongTinTaiKhoanController.java
+++ UIUX/src/main/java/com/example/uiux/admin_ThongTinTaiKhoanController.java
@@ -1,10 +1,51 @@
 package com.example.uiux;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class admin_ThongTinTaiKhoanController extends admin_ChuyenTrangController {
     @FXML
     private void LogoutButtonClicked(){
         Main.loadScene("login.fxml");
     }
+
+    @FXML
+    private Button XacNhanButton;
+
+    @FXML
+    private Button DoiMatKhauButton;
+
+    @FXML
+    private TextField PasswordTextField;
+
+    @FXML
+    private Text PasswordText;
+
+
+    public void initialize() {
+        // Set XacNhanButton initially invisible
+        XacNhanButton.setVisible(false);
+    }
+
+    @FXML
+    private void handleDoiMatKhauButtonClicked() {
+        // Hide the PasswordText
+        PasswordText.setVisible(false);
+
+        // Show the PasswordTextField
+        PasswordTextField.setVisible(true);
+        XacNhanButton.setVisible(true);
+
+        // Set focus to the PasswordTextField
+        PasswordTextField.requestFocus();
+    }
+
+    @FXML
+    private void handleXacNhanButtonClicked() {
+        // Handle the action when XacNhanButton is clicked
+        // You can add your custom logic here
+    }
+
 }
--- UIUX/src/main/java/com/example/uiux/loginController.java
+++ UIUX/src/main/java/com/example/uiux/loginController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
 public class loginController {
 
@@ -12,6 +13,16 @@ public class loginController {
 
     @FXML
     private TextField PasswordTextField;
+
+    @FXML
+    private void initialize() {
+        // Add a key pressed event listener for the PasswordTextField
+        PasswordTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                onLoginButtonClicked();
+            }
+        });
+    }
 
     @FXML
     private void onLoginButtonClicked() {
@@ -23,8 +34,9 @@ public class loginController {
             Main.loadScene("admin_TrangChu.fxml");
         } else if (username.equals("manager") && password.equals("1")) {
             Main.loadScene("Manager_TrangChu.fxml");
-        }
-        else{
+        } else if (username.equals("canho101") && password.equals("1")) {
+            Main.loadScene("citizen_TrangChu.fxml");
+        } else{
             // Handle incorrect credentials (e.g., show an error message)
             Main.showAlert("Thông tin đăng nhập không hợp lệ","Lỗi đăng nhập", Alert.AlertType.ERROR);
         }
