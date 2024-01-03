4bd0a9ad2cbdd16e18acbf0e5b92ebd9cab6b076
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2024-01-03 21:40:14 +0700

    Update citizen_ChuyenTrangController.java

--- UIUX/src/main/java/com/example/uiux/citizen_ChuyenTrangController.java
+++ UIUX/src/main/java/com/example/uiux/citizen_ChuyenTrangController.java
@@ -1,5 +1,27 @@
 package com.example.uiux;
 
-public class citizen_ChuyenTrangController {
+import javafx.fxml.FXML;
 
+public class citizen_ChuyenTrangController {
+    @FXML
+    private void TrangChu(){Main.loadScene("citizen_TrangChu.fxml"); }
+    @FXML
+    private void ThongTinCanHo(){
+        Main.loadScene("citizen_ThongTinCanHo.fxml");
+    }
+    @FXML
+    private void HoaDon(){
+        Main.loadScene("citizen_HoaDon.fxml");
+    }
+    private void ThongTinTaiKhoan(){
+        Main.loadScene("citizen_ThongTinTaiKhoan.fxml");
+    }
+    @FXML
+    private void TinNhan(){
+        Main.loadScene("citizen_Chat.fxml");
+    }
+    @FXML
+    private void YeuCauChinhSua(){
+        Main.loadScene("YeuCauChinhSua.fxml");
+    }
 }
