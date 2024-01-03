ac1f9362eec3a148f80df7f41328757d4187e04a
Halcolt <billtrinh19@gmail.com>
2024-01-03 12:13:56 +0700

    add

--- UIUX/src/main/java/com/example/uiux/admin_ChuyenTrangController.java
+++ UIUX/src/main/java/com/example/uiux/admin_ChuyenTrangController.java
@@ -7,14 +7,27 @@ public class admin_ChuyenTrangController {
     private void TrangChu(){
         Main.loadScene("admin_TrangChu.fxml");
     }
+
     @FXML
     private void ThongTinTaiKhoan(){
         Main.loadScene("admin_ThongTinTaiKhoan.fxml");
     }
+
     @FXML
     private void ThongTinNhanKhau(){
         Main.loadScene("admin_ThongTinNhanKhau.fxml");
     }
+
+    @FXML
+    private void TaoThongBao(){
+        Main.loadScene("TaoThongBao.fxml");
+    }
+
+    @FXML
+    private void QuanLiSuKien(){
+        Main.loadScene("QuanLiSuKien.fxml");
+    }
+    
     @FXML
     private void TinNhan(){
         Main.loadScene("Chat.fxml");
