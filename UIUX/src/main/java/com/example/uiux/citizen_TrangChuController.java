d80c1db8fb8bdfcfb8ba6cab0d6041db5bb75020
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2024-01-04 09:34:10 +0700

    Update citizen_TrangChuController.java

--- UIUX/src/main/java/com/example/uiux/citizen_TrangChuController.java
+++ UIUX/src/main/java/com/example/uiux/citizen_TrangChuController.java
@@ -1,4 +1,7 @@
 package com.example.uiux;
 
 public class citizen_TrangChuController extends citizen_ChuyenTrangController{
+  private void avatarClicked(){
+        Main.loadScene("citizen_ThongTinTaiKhoan.fxml");
+    }
 }
