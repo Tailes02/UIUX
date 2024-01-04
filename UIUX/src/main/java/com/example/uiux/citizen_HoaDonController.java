58f9f5f81b9ab72eaab25e5dd1b9314e3bd2256b
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2024-01-04 09:50:29 +0700

    Update citizen_HoaDonController.java

--- UIUX/src/main/java/com/example/uiux/citizen_HoaDonController.java
+++ UIUX/src/main/java/com/example/uiux/citizen_HoaDonController.java
@@ -1,4 +1,10 @@
 package com.example.uiux;
 
+import javafx.fxml.FXML;
+
 public class citizen_HoaDonController extends citizen_ChuyenTrangController{
+    @FXML
+    private void avatarClicked(){
+        Main.loadScene("citizen_ThongTinTaiKhoan.fxml");
+    }
 }
