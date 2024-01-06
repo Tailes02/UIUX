09629fc4e09859483523a129071efba9b6b15339
Tailes02 <taihuule02@gmail.com>
2024-01-06 17:30:44 +0700

    commit

--- UIUX/src/main/java/com/example/uiux/Main.java
+++ UIUX/src/main/java/com/example/uiux/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         com.example.uiux.Main.primaryStage = primaryStage; // Assign the primaryStage to the static field git
-        loadScene("Manager_HoaDonChiTiet.fxml");
+        loadScene("login.fxml");
     }
 
     public static void showAlert(String message, String title, Alert.AlertType type) {
