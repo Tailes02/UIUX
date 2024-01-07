81cf507da6f133ab81f5382f7836b87b20e05f96
Tailes02 <taihuule02@gmail.com>
2024-01-08 03:27:52 +0700

    commit

---
+++ UIUX/src/main/java/com/example/uiux/Manager_SuaHoaDonController.java
@@ -0,0 +1,18 @@
+package com.example.uiux;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+public class Manager_SuaHoaDonController {
+
+    @FXML
+    public void handleQuayLai() {
+        Main.loadScene("Manager_HoaDonChiTiet.fxml");
+    }
+    @FXML
+    public void handleXacNhan() {
+        Main.showAlert("Sửa hóa đơn thành công",null, Alert.AlertType.INFORMATION);
+        Main.loadScene("Manager_HoaDonChiTiet.fxml");
+
+    }
+}
