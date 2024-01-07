b34e7e32741ef47642665b020e5fa92249d12fe7
Halcolt <billtrinh19@gmail.com>
2024-01-07 20:36:43 +0700

    add

--- UIUX/src/main/java/com/example/uiux/admin_ThongTinChiTietCanHoController.java
+++ UIUX/src/main/java/com/example/uiux/admin_ThongTinChiTietCanHoController.java
@@ -1,6 +1,10 @@
 package com.example.uiux;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class admin_ThongTinChiTietCanHoController extends admin_ChuyenTrangController{
 
@@ -12,5 +16,22 @@ public class admin_ThongTinChiTietCanHoController extends admin_ChuyenTrangContr
     @FXML
     private void handleButtonHuy(){
         Main.loadScene("admin_ThongTinCanHo.fxml");
+    }
+
+    @FXML
+    private void handleButtonXacNhan(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Xác nhận thay đổi thông tin ?");
+
+        ButtonType buttonTypeConfirm = new ButtonType("Xác nhận");
+        ButtonType buttonTypeCancel = new ButtonType("Hủy");
+        alert.getButtonTypes().setAll(buttonTypeConfirm, buttonTypeCancel);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == buttonTypeConfirm) {
+            Main.showAlert("Thay đổi thông tin thành công",null, Alert.AlertType.INFORMATION);
+            Main.loadScene("admin_ThongTinChiTietCanHoSauXacNhan.fxml");
+        }
+
     }
 }
