1408b9ac1995ced604bc98172fbe4b88401760e6
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2024-01-07 20:19:00 +0700

    Update admin_ThemTaiKhoanController.java

--- UIUX/src/main/java/com/example/uiux/admin_ThemTaiKhoanController.java
+++ UIUX/src/main/java/com/example/uiux/admin_ThemTaiKhoanController.java
@@ -1,6 +1,51 @@
 package com.example.uiux;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class admin_ThemTaiKhoanController extends admin_ChuyenTrangController{
+    @FXML
+    TextField textFieldTaiKhoan;
+    @FXML
+    TextField textFieldHoTen;
+    @FXML
+    TextField textFieldCCCD;
+    @FXML
+    TextField textFieldMatKhau;
+    @FXML
+    TextField textFieldCanHo;
+    @FXML
+    TextField textFieldSoDienThoai;
+
+    public void handleXacNhanButton() {
+        String taiKhoan = textFieldTaiKhoan.getText();
+        String hoTen = textFieldHoTen.getText();
+        String cccd = textFieldCCCD.getText();
+        String matKhau = textFieldMatKhau.getText();
+        String CanHo = textFieldCanHo.getText();
+        String SoDienThoai = textFieldSoDienThoai.getText();
+
+        if (taiKhoan.isEmpty() || hoTen.isEmpty() || cccd.isEmpty() || matKhau.isEmpty() || CanHo.isEmpty() || SoDienThoai.isEmpty()) {
+            showRequiredFieldsAlert(); // Hiển thị cảnh báo nếu có trường chưa điền
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Thành công !");
+            alert.setHeaderText(null);
+            alert.setContentText("Thêm nhân khẩu thành công !");
+        }
+    }
+
+    private void showRequiredFieldsAlert() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Cảnh báo");
+        alert.setHeaderText(null);
+        alert.setContentText("Vui lòng điền đầy đủ các trường bắt buộc (*)!");
+
+        alert.showAndWait();
+    }
+
+
     public void handleHuyButton(){
         Main.loadScene("admin_ThongTinCanHo.fxml");
     }
