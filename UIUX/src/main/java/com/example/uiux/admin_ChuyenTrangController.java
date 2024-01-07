97798b6549f3dc4f03504e1029cd027a959808b9
Remilia Scarlet <lethanhbinhkts@gmail.com>
2024-01-08 00:12:16 +0700

    fix thông báo cho manager và admin

--- UIUX/src/main/java/com/example/uiux/admin_ChuyenTrangController.java
+++ UIUX/src/main/java/com/example/uiux/admin_ChuyenTrangController.java
@@ -1,8 +1,38 @@
 package com.example.uiux;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class admin_ChuyenTrangController {
+    public void showThongBaoPopup() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("citizen_ThongBao.fxml"));
+            Parent root = loader.load();
+
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+            popupStage.setTitle("Thông Báo");
+
+            Scene scene = new Scene(root);
+            popupStage.setScene(scene);
+
+
+            popupStage.setOnCloseRequest(event -> {
+                event.consume();
+
+                popupStage.close();
+            });
+
+            // Show the popup
+            popupStage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @FXML
     private void TrangChu(){
         Main.loadScene("admin_TrangChu.fxml");
