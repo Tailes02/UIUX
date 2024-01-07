27f4305748faf0f7a47d8e2e0f546c8097068c50
Remilia Scarlet <lethanhbinhkts@gmail.com>
2024-01-07 14:53:29 +0700

    added news controller

--- UIUX/src/main/java/com/example/uiux/NewsController.java
+++ UIUX/src/main/java/com/example/uiux/NewsController.java
@@ -2,37 +2,69 @@ package com.example.uiux;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-public class HeadLineController {
+public class NewsController {
     @FXML
-    private Label headline;
+    private Label headline1;
     @FXML
-    private TextFlow newsContent;
+    private Label headline2;
     @FXML
-    private Text news;
+    private Label headline3;
+    @FXML
+    private TextFlow newsContent1;
+    @FXML
+    private TextFlow newsContent2;
+    @FXML
+    private TextFlow newsContent3;
+    @FXML
+    private Text news1;
+    @FXML
+    private Text news2;
+    @FXML
+    private Text news3;
 
-    private String h;
-    private String n;
+    private String h1;
+    private String h2;
+    private String h3;
+    private String n1;
+    private String n2;
+    private String n3;
 
-    public HeadLineController(String h, String n){
-        this.h=h;
-        this.n=n;
+    public NewsController(String h1, String h2, String h3, String n1, String n2, String n3){
+        this.h1=h1;
+        this.h2=h2;
+        this.h3=h3;
+        this.n1=n1;
+        this.n2=n2;
+        this.n3=n3;
     }
     @FXML
     public void initialize(){
-        if (h.length()>15)
-            this.headline.setText(h.substring(0,15)+"...");
+        if (h1.length()>15)
+            this.headline1.setText(h1.substring(0,15)+"...");
         else
-            this.headline.setText(h);
-        news.setText(n);
-        news.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
-        newsContent.getChildren().add(news);
+            this.headline1.setText(h1);
+        if (h2.length()>15)
+            this.headline2.setText(h2.substring(0,15)+"...");
+        else
+            this.headline2.setText(h2);
+        if (h3.length()>15)
+            this.headline3.setText(h3.substring(0,15)+"...");
+        else
+            this.headline3.setText(h3);
+        news1.setText(n1);
+        news1.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
+        newsContent1.getChildren().add(news1);
+        news2.setText(n2);
+        news2.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
+        newsContent2.getChildren().add(news2);
+        news3.setText(n3);
+        news3.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
+        newsContent3.getChildren().add(news3);
         }
 
     }
--- UIUX/src/main/java/com/example/uiux/citizen_TrangChuController.java
+++ UIUX/src/main/java/com/example/uiux/citizen_TrangChuController.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -22,19 +20,23 @@ public class citizen_TrangChuController extends citizen_ChuyenTrangController{
     @FXML
     private ScrollPane newsScroll;
     @FXML
+    private ScrollPane headlineScroll;
+    @FXML
     private Label headline;
     @FXML
     private TextFlow newsContent;
     @FXML
     private Text news;
     @FXML
+    private VBox headlineColumn;
+    @FXML
     private void initialize(){
         newsScroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        headlineScroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         loadNews("Chung cư iHopT tổ chức lễ giáng sinh","Giáng sinh 2023", false);
         loadNews("Ban quản lý tổ chức khen thưởng cho học sinh xuất sắc","Tổng kết học kì 1 2023", false);
-        news.setText("Chung cư iHopT tô chức lễ giáng sinh cho toàn bộ trẻ em trong khu chung cư. Địa điểm diễn ra ở sảnh chung cư");
-        news.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
-        newsContent.getChildren().add(news);
+        loadHeadlines("Giáng sinh 2023","Chung cư iHopT tô chức lễ giáng sinh cho toàn bộ trẻ em trong khu chung cư. Địa điểm diễn ra ở sảnh chung cư");
+        loadNewsContent("Trung thu 2023","Quyên góp ủng hộ","Trao trưởng","Chung cư iHoptT tổ chức trung thu cho toàn bộ trẻ em trong khu chung cư","Chung cư iHopT tổ chức quyên góp ủng hộ nạn nhân vụ cháy chung cư Khương Hạ","Tuyên dương các em học sinh xuất sắc học kì 1 năm học 2023-2024");
     }
 
     private void loadNews(String preview, String user, Boolean highlight){
@@ -49,13 +51,25 @@ public class citizen_TrangChuController extends citizen_ChuyenTrangController{
             e.printStackTrace();
         }
     }
+    private void loadNewsContent(String headline1, String headline2, String headline3, String newscontent1, String newscontent2, String newscontent3){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("TinTuc_news.fxml"));
+            NewsController newsController = new NewsController(headline1, headline2, headline3, newscontent1, newscontent2, newscontent3);
+            loader.setController(newsController);
+            loader.load();
+            headlineColumn.getChildren().add(loader.getRoot());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
     private void loadHeadlines(String headline, String content) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Chat_user.fxml"));
-            //ChatUserController chatUserController = new ChatUserController(preview, user, highlight);
-            //loader.setController(chatUserController);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("TinTuc_headline.fxml"));
+            HeadLineController headlineController = new HeadLineController(headline, content);
+            loader.setController(headlineController);
             loader.load();
-            newsColumn.getChildren().add(loader.getRoot());
+            headlineColumn.getChildren().add(loader.getRoot());
 
         }catch (IOException e){
             e.printStackTrace();
